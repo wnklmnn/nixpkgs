359615859c8ac2906ddf1c791fc44c3771762490
Pascal Winkelmann <pascal@wnklmnn.de>
2019-12-23 14:59:32 +0100

    Removed deps to autoconf and automake111 Added dependency "autoreconfHook" moved build-time dependencies to nativeBuildInput

--- pkgs/applications/networking/browsers/dillo/default.nix
+++ pkgs/applications/networking/browsers/dillo/default.nix
@@ -3,7 +3,7 @@
 , openssl
 , libjpeg, libpng
 , perl
-, libXcursor, libXi, libXinerama, which,automake111x, autoconf }:
+, libXcursor, libXi, libXinerama, which, autoreconfHook }:
 
 stdenv.mkDerivation rec {
   version = "3.0.5";
@@ -13,9 +13,8 @@ stdenv.mkDerivation rec {
     url = "https://www.dillo.org/download/${pname}-${version}.tar.bz2";
     sha256 = "12ql8n1lypv3k5zqgwjxlw1md90ixz3ag6j1gghfnhjq3inf26yv";
   };
-
-  buildInputs = with stdenv.lib;
-  [ which perl fltk openssl libjpeg libpng libXcursor libXi libXinerama automake111x autoconf ];
+  nativeBuildInputs = with stdenv.lib; [ which autoreconfHook perl ];
+  buildInputs = with stdenv.lib; [ fltk openssl libjpeg libpng libXcursor libXi libXinerama ];
   patches = [ ./fix-OpenSSL-1.1-detection.patch];
   configureFlags = [ "--enable-ssl" ];
 
